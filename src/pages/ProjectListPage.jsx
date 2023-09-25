497537610f061dac80df3f3f34b66ee9d9041cf9
Mirabela Tiugan <mirabelatiugan@gmail.com>
2023-09-25 15:34:49 +0200

    practice

---
+++ src/pages/ProjectListPage.jsx
@@ -0,0 +1,43 @@
+// src/pages/ProjectListPage.jsx
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import ProjectCard from "../components/ProjectCard";
+import AddProject from "../components/AddProject"; //  <== IMPORT
+
+const API_URL = "http://localhost:5005";
+
+function ProjectListPage() {
+  const [projects, setProjects] = useState([]);
+
+  const getAllProjects = () => {
+    axios
+      .get(`${API_URL}/api/projects`)
+      .then((response) => {
+        console.log(response.data); // Log the response data
+        setProjects(response.data)})
+      .catch((error) => console.log(error));
+  };
+
+  // We set this effect will run only once, after the initial render
+  // by setting the empty dependency array - []
+  useEffect(() => {
+    getAllProjects();
+  }, []);
+
+  return (
+    <div className="ProjectListPage">
+      
+      <AddProject refreshProjects={getAllProjects} />
+      
+      {/* Below: UPDATE */}
+      { projects.map((project) => (
+        <ProjectCard key={project._id} {...project} />
+      ))}     
+       
+    </div>
+  );
+}
+
+export default ProjectListPage;
